b93f85fd7c7963334dad89f1b28125c7062fe79a
tyrnor <carlosyanez997@gmail.com>
2024-03-16 21:59:07 +0100

    Create AddMessageUseCase

---
+++ app/src/main/java/com/example/chatapp/domain/usecase/database/AddMessageUseCase.kt
@@ -0,0 +1,10 @@
+package com.example.chatapp.domain.usecase.database
+
+import com.example.chatapp.domain.repository.DatabaseRepository
+import javax.inject.Inject
+
+class AddMessageUseCase @Inject constructor(private val databaseRepository: DatabaseRepository) {
+    suspend operator fun invoke(chatId: String, senderId: String, text: String): Result<Unit> {
+        return databaseRepository.addMessage(chatId, senderId, text)
+    }
+}
--- app/src/main/java/com/example/chatapp/domain/usecase/database/AddUserUseCase.kt
+++ app/src/main/java/com/example/chatapp/domain/usecase/database/AddUserUseCase.kt
@@ -4,8 +4,8 @@ import com.example.chatapp.data.model.UserInformation
 import com.example.chatapp.domain.repository.DatabaseRepository
 import javax.inject.Inject
 
-class AddUserUseCase @Inject constructor(private val databaseRepository: DatabaseRepository){
-    suspend operator fun invoke(uid: String, userInfo: UserInformation) : Result<Unit> {
+class AddUserUseCase @Inject constructor(private val databaseRepository: DatabaseRepository) {
+    suspend operator fun invoke(uid: String, userInfo: UserInformation): Result<Unit> {
         return databaseRepository.addUser(uid, userInfo)
     }
 }
