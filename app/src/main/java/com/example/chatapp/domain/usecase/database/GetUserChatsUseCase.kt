a022f50cacc5ae85b43617cf808c59a13a5aa3d7
tyrnor <carlosyanez997@gmail.com>
2024-03-21 15:32:21 +0100

    Create GetUserChatsUseCase

---
+++ app/src/main/java/com/example/chatapp/domain/usecase/database/GetUserChatsUseCase.kt
@@ -0,0 +1,12 @@
+package com.example.chatapp.domain.usecase.database
+
+import com.example.chatapp.data.model.Chat
+import com.example.chatapp.domain.repository.DatabaseRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetUserChatsUseCase @Inject constructor(private val databaseRepository: DatabaseRepository){
+    suspend operator fun invoke() : Flow<List<Chat>> {
+        return databaseRepository.getUserChats()
+    }
+}
