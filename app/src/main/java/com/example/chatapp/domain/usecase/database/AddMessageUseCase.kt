fc4d32d192740d12690690c0a9d5461e2f5ba7aa
tyrnor <carlosyanez997@gmail.com>
2024-03-25 21:45:28 +0100

    Update AddMessageUseCase

--- app/src/main/java/com/example/chatapp/domain/usecase/database/AddMessageUseCase.kt
+++ app/src/main/java/com/example/chatapp/domain/usecase/database/AddMessageUseCase.kt
@@ -4,7 +4,7 @@ import com.example.chatapp.domain.repository.DatabaseRepository
 import javax.inject.Inject
 
 class AddMessageUseCase @Inject constructor(private val databaseRepository: DatabaseRepository) {
-    suspend operator fun invoke(chatId: String, currentUserId: String, text: String): Result<Unit> {
-        return databaseRepository.addMessage(chatId, currentUserId, text)
+    suspend operator fun invoke(chatId: String, currentUserId: String, otherUserId: String, text: String): Result<Unit> {
+        return databaseRepository.addMessage(chatId, currentUserId,otherUserId, text)
     }
 }
