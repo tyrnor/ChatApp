417f774a97bcdf1abe5012ae7070240b7bb27690
tyrnor <carlosyanez997@gmail.com>
2024-03-29 21:49:07 +0100

    Add lastMessage to be shown in ChatsScreen

--- app/src/main/java/com/example/chatapp/ui/view/home/ChatsScreen.kt
+++ app/src/main/java/com/example/chatapp/ui/view/home/ChatsScreen.kt
@@ -1,21 +1,28 @@
 package com.example.chatapp.ui.view.home
 
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.example.chatapp.ui.navigation.Chat
 import com.example.chatapp.ui.theme.AppTheme
+import com.example.chatapp.ui.theme.Grey
+import com.example.chatapp.ui.theme.LightGrey
 import com.example.chatapp.ui.viewmodel.ChatsViewModel
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -29,14 +36,30 @@ fun ChatsScreen(navController: NavController) {
     LaunchedEffect(chats) {
         chatsViewModel.getUserChats(currentUserId)
     }
-    LazyColumn(modifier = Modifier
-        .fillMaxSize()
-        .background(AppTheme.colorScheme.background)) 
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(AppTheme.colorScheme.background)
+    )
     {
-        items(chats){
-            Text(text = it.otherUserDisplayName, modifier = Modifier.clickable {
-                navController.navigate(Chat.route + "/${it.otherUserId}")
-            })
+        items(chats) {
+            Column(modifier = Modifier
+                .fillMaxWidth()
+                .clickable {
+                    navController.navigate(Chat.route + "/${it.otherUserId}")
+                }) {
+                Text(text = it.otherUserDisplayName)
+                Text(text = it.lastMessage)
+                Divider(
+                    modifier = Modifier
+                        .background(LightGrey)
+                        .height(1.dp)
+                        .align(Alignment.CenterHorizontally)
+                        .fillMaxWidth(0.99f)
+                )
+            }
+
+
         }
     }
 }
