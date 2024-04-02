eb1b6bcfcae89ce85dc72139ef90f2daf5e76a88
tyrnor <carlosyanez997@gmail.com>
2024-04-02 23:17:49 +0200

    Add image profile to SettingsScreen

--- app/src/main/java/com/example/chatapp/ui/view/home/SettingsScreen.kt
+++ app/src/main/java/com/example/chatapp/ui/view/home/SettingsScreen.kt
@@ -3,9 +3,16 @@ package com.example.chatapp.ui.view.home
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Circle
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.chatapp.ui.theme.AppTheme
 
 @Composable
@@ -13,6 +20,15 @@ fun SettingsScreen() {
     Column(modifier = Modifier
         .fillMaxSize()
         .background(AppTheme.colorScheme.background)) {
+        Icon(
+            imageVector = Icons.Filled.Circle,
+            contentDescription = "Image profile",
+            modifier = Modifier
+                .size(128.dp)
+                .align(Alignment.CenterHorizontally)
+                .padding(AppTheme.size.small),
+            tint = AppTheme.colorScheme.icons
+        )
         Text(text = "Settings")
     }
 }
