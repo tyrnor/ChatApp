6d5c9838988a756c4c752c9a8cbdee1a9da91d5d
tyrnor <carlosyanez997@gmail.com>
2024-04-03 04:02:11 +0200

    Add user displayName to SettingsScreen

--- app/src/main/java/com/example/chatapp/ui/view/home/SettingsScreen.kt
+++ app/src/main/java/com/example/chatapp/ui/view/home/SettingsScreen.kt
@@ -12,14 +12,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.capitalize
 import androidx.compose.ui.unit.dp
+import com.example.chatapp.data.model.AuthenticatedUser
 import com.example.chatapp.ui.theme.AppTheme
 
 @Composable
-fun SettingsScreen() {
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .background(AppTheme.colorScheme.background)) {
+fun SettingsScreen(user: AuthenticatedUser) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(AppTheme.colorScheme.background)
+    ) {
         Icon(
             imageVector = Icons.Filled.Circle,
             contentDescription = "Image profile",
@@ -29,6 +33,11 @@ fun SettingsScreen() {
                 .padding(AppTheme.size.small),
             tint = AppTheme.colorScheme.icons
         )
-        Text(text = "Settings")
+        Text(
+            text = user.displayName?.replaceFirstChar {
+                it.uppercase()
+            } ?: "Username not available",
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
     }
 }
